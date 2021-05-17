32c6f7ba43a9c662e51fc5821bf0c0fccb76ada5
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2021-05-17 01:34:55 -0400

    trial

--- trial.py
+++ trial.py
@@ -1,3 +1,25 @@
+from keys import clientid, usr, pw, token, usa
 import praw
 
-print('hello')
+reddit = praw.Reddit(
+    client_id = clientid,
+    client_secret = token, 
+    user_agent = usa, 
+    username = usr,     
+    password = pw
+)
+
+subreddit = reddit.subreddit('wallstreetbets')
+
+top_subreddit = subreddit.new(limit=10)
+
+title_list = []
+
+for post in top_subreddit:
+    title = post.title
+    title_text = title.split()
+    title_list.append(title_text)
+
+print(title_list)
+
+### it works!! 
